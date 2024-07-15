452205081b6792f3d3dee54f02caab26066058af
Gyanendra Mishra <anomaly.the@gmail.com>
2024-07-15 14:40:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-import requests
-import re
+import json
 
-def setup(manifest, service_name):
-    response = requests.get("https://ident.me")
-    if response.status_code != 200:
-        raise Exception("an unexpected error occurred")
-    updated_manifest = manifest.replace("ip_addr", response.text)
-    return updated_manifest
+REPLACED = "the-text-has-been-replaced"
 
-
-def teardown(manifest, service_name):
-    ip_pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
+def create_flow(service_spec, deployment_spec, flow_uuid, text_to_replace):
+    deployment_spec = json.loads(deployment_spec)
     
-    updated_manifest = re.sub(ip_pattern, "ip_addr", manifest)
+    deployment_spec['Template']['Labels']['app'] = deployment_spec['Template']['Labels']['app'].replace(text_to_replace, REPLACED)
+    deployment_spec['Selector']['MatchLabels']['app'] = deployment_spec['Selector']['MatchLabels']['app'].replace(text_to_replace, REPLACED)
+    deployment_spec['Template']['Spec']['Containers'][0]['Name'] = deployment_spec['Template']['Spec']['Containers'][0]['Name'].replace(text_to_replace, REPLACED)
     
-    return updated_manifest
+    config_map = {
+        "original_text": text_to_replace
+    }
+    
+    return {
+        "service_spec": service_spec,
+        "config_map": json.dumps(config_map)
+    }
+
+def delete_flow(config_map, flow_uuid):
+    return None
