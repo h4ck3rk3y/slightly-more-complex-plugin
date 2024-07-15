072a559601b36ab8dcf490df865dd62437152dfa
Gyanendra Mishra <anomaly.the@gmail.com>
2024-07-15 15:06:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ REPLACED = "the-text-has-been-replaced"
 def create_flow(service_spec, deployment_spec, flow_uuid, text_to_replace):
     deployment_spec = json.loads(deployment_spec)
     
-    deployment_spec['Template']['Labels']['app'] = deployment_spec['Template']['Labels']['app'].replace(text_to_replace, REPLACED)
-    deployment_spec['Selector']['MatchLabels']['app'] = deployment_spec['Selector']['MatchLabels']['app'].replace(text_to_replace, REPLACED)
-    deployment_spec['Template']['Spec']['Containers'][0]['Name'] = deployment_spec['Template']['Spec']['Containers'][0]['Name'].replace(text_to_replace, REPLACED)
+    deployment_spec['template']['labels']['app'] = deployment_spec['template']['labels']['app'].replace(text_to_replace, REPLACED)
+    deployment_spec['selector']['matchLabels']['app'] = deployment_spec['selector']['matchLabels']['app'].replace(text_to_replace, REPLACED)
+    deployment_spec['template']['spec']['containers'][0]['name'] = deployment_spec['template']['spec']['containers'][0]['Name'].replace(text_to_replace, REPLACED)
     
     config_map = {
         "original_text": text_to_replace
